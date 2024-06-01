6bed7cace2e06f5e310d07a42ca9a7567df72612
Komlan Samati <kn.samati@gmail.com>
2024-06-01 16:25:51 +0000

    premier commit

---
+++ import_data.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(robotoolbox)
+library(janitor)
+
+kobo_setup(url = "https://kf.kobotoolbox.org/",
+           token = Sys.getenv('MY_TOKEN'))
+
+
+asset <- kobo_asset(Sys.getenv('MY_UID'))
+asset |>
+    kobo_asset() |>
+    kobo_data() |>
+    janitor::clean_names() |>
+    write_rds("survey_data.rds")
